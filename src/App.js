57007c979b5bbab8cabc7067822789eadcb98ca7
Lefteris Eleftheriou <lefteris.elefth@gmail.com>
2023-04-22 15:14:58 +0300

    Route home/about pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import Header from "./components/common/Header";
+const App = () => {
 
-function App() {
+  
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
