8f021bc0ece2ce6492aba17475442fa973077a4c
AntonyRoussos <tonyroussos92@gmail.com>
2023-07-27 00:08:55 +0300

    navlink

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import "./header.css";
 const Header = () => {
   return (
     <nav>
       <ul className="navbar">
-        <li className="navlink">
-          <NavLink style={({ isActive }) => {
-    return {
-      textDecoration: "none",
-      color: isActive ? "white" : "black",
-    };
-  }} to="/">Home</NavLink>
+        <li>
+          <NavLink
+            className="navlink"
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "white" : "black",
+              };
+            }}
+            to="/"
+          >
+            Home
+          </NavLink>
         </li>
-        <li  className="navlink">
-          <NavLink style={({ isActive }) => {
-    return {
-      textDecoration: "none",
-      color: isActive ? "white" : "black",
-    };
-  }}to="/about">About</NavLink>
+        <li>
+          <NavLink
+            className="navlink"
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "white" : "black",
+              };
+            }}
+            to="/about"
+          >
+            About
+          </NavLink>
         </li>
       </ul>
     </nav>
